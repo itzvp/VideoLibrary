94cf6722dfbe99738441d160e0e85255dcf0d9c6
itzvp <125799424+itzvp@users.noreply.github.com>
2024-07-04 03:24:09 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,14 @@
-// "use client";
-// import dynamic from "next/dynamic";
-// import React, { useState } from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Navbar from "@/components/Navbar";
-// import HomePage from "@/pages/HomePage";
-// import BookmarkedPage from "@/pages/BookmarkedPage";
-// import { Video } from "@/types/Video";
-
-// const App: React.FC = () => {
-//   const [videos, setVideos] = useState<Video[]>([]);
-
-//   const handleAddVideo = (video: Video) => {
-//     setVideos([...videos, video]);
-//   };
-
-//   const handleToggleBookmark = (id: string) => {
-//     setVideos(
-//       videos.map((video) =>
-//         video.id === id
-//           ? { ...video, isBookmarked: !video.isBookmarked }
-//           : video
-//       )
-//     );
-//   };
-
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={
-//             <HomePage
-//               videos={videos}
-//               onAddVideo={handleAddVideo}
-//               onToggleBookmark={handleToggleBookmark}
-//             />
-//           }
-//         />
-//         <Route
-//           path="/bookmarked"
-//           element={
-//             <BookmarkedPage
-//               videos={videos}
-//               onToggleBookmark={handleToggleBookmark}
-//             />
-//           }
-//         />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 "use client";
+import dynamic from "next/dynamic";
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import HomePage from "@/pages/HomePage";
 import BookmarkedPage from "@/pages/BookmarkedPage";
 import { Video } from "@/types/Video";
-import { useRouter } from "next/navigation";
 
 const App: React.FC = () => {
   const [videos, setVideos] = useState<Video[]>([]);
-  const router = useRouter();
 
   const handleAddVideo = (video: Video) => {
     setVideos([...videos, video]);
@@ -80,42 +24,98 @@ const App: React.FC = () => {
     );
   };
 
-  const renderPage = () => {
-    const path = router.pathname;
-
-    switch (path) {
-      case "/":
-        return (
-          <HomePage
-            videos={videos}
-            onAddVideo={handleAddVideo}
-            onToggleBookmark={handleToggleBookmark}
-          />
-        );
-      case "/bookmarked":
-        return (
-          <BookmarkedPage
-            videos={videos}
-            onToggleBookmark={handleToggleBookmark}
-          />
-        );
-      default:
-        return (
-          <HomePage
-            videos={videos}
-            onAddVideo={handleAddVideo}
-            onToggleBookmark={handleToggleBookmark}
-          />
-        );
-    }
-  };
-
   return (
-    <div>
+    <Router>
       <Navbar />
-      {renderPage()}
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              videos={videos}
+              onAddVideo={handleAddVideo}
+              onToggleBookmark={handleToggleBookmark}
+            />
+          }
+        />
+        <Route
+          path="/bookmarked"
+          element={
+            <BookmarkedPage
+              videos={videos}
+              onToggleBookmark={handleToggleBookmark}
+            />
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+// "use client";
+// import React, { useState } from "react";
+// import Navbar from "@/components/Navbar";
+// import HomePage from "@/pages/HomePage";
+// import BookmarkedPage from "@/pages/BookmarkedPage";
+// import { Video } from "@/types/Video";
+// import { usePathname } from "next/navigation";
+
+// const App: React.FC = () => {
+//   const [videos, setVideos] = useState<Video[]>([]);
+//   const path = usePathname(); // Access path directly in the component
+
+//   const handleAddVideo = (video: Video) => {
+//     setVideos([...videos, video]);
+//   };
+
+//   const handleToggleBookmark = (id: string) => {
+//     setVideos(
+//       videos.map((video) =>
+//         video.id === id
+//           ? { ...video, isBookmarked: !video.isBookmarked }
+//           : video
+//       )
+//     );
+//   };
+
+//   // Render content based on the path directly in the component
+//   let pageContent;
+//   switch (path) {
+//     case "/":
+//       pageContent = (
+//         <HomePage
+//           videos={videos}
+//           onAddVideo={handleAddVideo}
+//           onToggleBookmark={handleToggleBookmark}
+//         />
+//       );
+//       break;
+//     case "/bookmarked":
+//       pageContent = (
+//         <BookmarkedPage
+//           videos={videos}
+//           onToggleBookmark={handleToggleBookmark}
+//         />
+//       );
+//       break;
+//     default:
+//       pageContent = (
+//         <HomePage
+//           videos={videos}
+//           onAddVideo={handleAddVideo}
+//           onToggleBookmark={handleToggleBookmark}
+//         />
+//       );
+//       break;
+//   }
+
+//   return (
+//     <div>
+//       <Navbar />
+//       {pageContent}
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,36 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const Navbar: React.FC = () => {
-//   return (
-//     <nav>
-//       <Link to="/">Home</Link>
-//       <Link to="/bookmarked">Bookmarked</Link>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React from "react";
-import Link from "next/link";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/bookmarked">Bookmarked</Link>
+      <Link to="/">Home</Link>
+      <Link to="/bookmarked">Bookmarked</Link>
     </nav>
   );
 };
 
 export default Navbar;
+
+// import React from "react";
+// import { usePathname } from "next/navigation";
+// import Link from "next/link";
+
+// const Navbar: React.FC = () => {
+//   const pathname = usePathname();
+//   return (
+//     <nav>
+//       <Link href="/" className={`link ${pathname === "/" ? "active" : ""}`}>
+//         Home
+//       </Link>
+//       <Link
+//         href="/bookmarked"
+//         className={`link ${pathname === "/bookmarked" ? "active" : ""}`}
+//       >
+//         Bookmarked
+//       </Link>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
